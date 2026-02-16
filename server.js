230535abcbc0f6605f80564f9cf261ddcd276b6a
aaronmanill <97758671+aaronmanill@users.noreply.github.com>
2026-02-15 23:48:01 -0800

    enabling vite dev mode

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:8080", // Replace with your React dev URL
+    origin: ["http://localhost:8080", "http://localhost:5173"],
     methods: ["GET", "POST"]
   }
 });
