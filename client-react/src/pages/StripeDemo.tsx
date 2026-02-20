9bc42b8ff4de4d4a73d738dc0110a0e986278eb3
aaronmanill <97758671+aaronmanill@users.noreply.github.com>
2026-02-19 16:05:13 -0800

    stripe connection isn't handled on front end

--- client-react/src/pages/StripeDemo.tsx
+++ client-react/src/pages/StripeDemo.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { loadStripe } from '@stripe/stripe-js';
+// import { loadStripe } from '@stripe/stripe-js';
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
-const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
+// const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
 
 const StripeDemo: React.FC = () => {
   const handleClick = async () => {
